8a52f6b5aad1f6eee5561c7f48177eb8fa177eac
alecmp <alessandro.campanello@gmail.com>
2023-07-21 15:36:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Introduction
+
+The implementation of BERT4Rec is based on:
+> **https://github.com/jaywonchung/BERT4Rec-VAE-Pytorch (66f0853) from Jaewon Chung**
+
+# Usage
+
+## Overall
+
+For practical purposes, the code can be executed from the Colab notebook:
+
+> **https://colab.research.google.com/drive/1bimVlU4NUNK2PTdyEuK_phSav4O_3VkU#scrollTo=6E2aXC4EClp-**
+
+Run `main.py` with arguments to train and/or test you model. There is a template file 'templates.py' available.
+
+```bash
+python main.py --template train_bert
+```
---
+++ datasets/ml_20m.py
@@ -0,0 +1,42 @@
+from .base import AbstractDataset
+
+import pandas as pd
+
+from datetime import date
+
+
+class ML20MDataset(AbstractDataset):
+    @classmethod
+    def code(cls):
+        return 'ml-20m'
+
+    @classmethod
+    def url(cls):
+        return 'http://files.grouplens.org/datasets/movielens/ml-20m.zip'
+        
+
+    @classmethod
+    def zip_file_content_is_folder(cls):
+        return True
+
+    @classmethod
+    def all_raw_file_names(cls):
+        return ['genome-scores.csv',
+                'genome-tags.csv',
+                'links.csv',
+                'movies.csv',
+                'ratings.csv',
+                'README.txt',
+                'tags.csv']
+
+    def load_ratings_df(self):
+        #folder_path = Path('alecmp/KeBERT4Rec/data/amazon_us_product.zip') 
+        #file_path = folder_path.joinpath('rating_50k.csv') #ratings
+        df = pd.read_csv('data/ratings.csv', header=0)
+        df.columns = ['uid', 'sid', 'rating', 'timestamp']
+        #c_file_path = folder_path.joinpath('output_50k.csv') #movies
+        mv_df = pd.read_csv('data/products.csv', header=0)
+        mv_df.columns = ['sid', 'title', 'categories']
+        mv_df = mv_df[["sid", "categories"]]
+        df = pd.merge(df, mv_df)
+        return df
