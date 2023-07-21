7308c706382f0f1d8d7adfb8ab4a571ceaa5c258
alecmp <alessandro.campanello@gmail.com>
2023-07-21 18:38:26 +0200

    fix 2

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,8 @@
-from .ml_1m import ML1MDataset
+#from .ml_1m import ML1MDataset
 from .ml_20m import ML20MDataset
 
 DATASETS = {
-    ML1MDataset.code(): ML1MDataset,
+    #ML1MDataset.code(): ML1MDataset,
     ML20MDataset.code(): ML20MDataset
 }
 
